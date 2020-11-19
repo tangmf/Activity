30570dc2c653ad176ab7a276cc8dab3661b7f26b
Tang Ming Feng <derpsnowtmf@gmail.com>
2020-11-19 16:39:52 +0800

    Add project files.

---
+++ Activity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
